1de1d0064e50224bcc117c921b27018aa3d54406
Taylor Covington <tcovingtondev@gmail.com>
2019-09-19 18:58:20 -0600

    grocery app created, displaying in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    items: [
+      { id: 1, name: 'Cereal', price: 2, in_cart: true },
+      { id: 1, name: 'Milk', price: 4, in_cart: true },
+      { id: 1, name: 'Bread', price: 10, in_cart: false }
+    ]
+  };
+
+  renderItems = () => {
+    const { items } = this.state;
+    return items.map( item => {
+      return (
+        <li key={item.id}>{item.name} {item.price}</li>
+      )
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <ul>
+          { this.renderItems() }
+        </ul>
+      </div>
+    );
+  }
 }
+
 
 export default App;
